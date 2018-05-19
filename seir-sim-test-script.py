959edbc38347bb397f07c8f2280974177a8685ea
Nick Terry <32914587+nick-terry@users.noreply.github.com>
2018-05-19 00:01:26 -0400

    Split test script into separate file

---
+++ seir-sim-test-script.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nick Terry
+
+A test script for seir-sim that generates a random contact network and runs the
+simulation
+"""
+import matplotlib.pyplot as plt
+from seirSim import all
+
+
+numNodes = 1000
+numEdges = 5000
+
+G = generateRandomGraph(numNodes, numEdges=numEdges)
+
+exposureRate = 10
+infectionRate = 3
+recoveryRate = .5
+
+log,stats = seirSim(G,exposureRate,infectionRate,recoveryRate,logSim=True, 
+              tallyFuncs=[numNodesInState(0),numNodesInState(1),
+                          numNodesInState(2),numNodesInState(3)])
+
+for i in range(4):
+    plt.plot(range(len(log)),stats[:,i])
+plt.legend(['Susceptible','Exposed','Infected','Removed'],loc='upper right')
--- seirSim.py
+++ seirSim.py
@@ -209,23 +209,3 @@ def numNodesInState(state):
         return np.sum(np.where(simState[0]==state,np.ones(n),np.zeros(n)))
     
     return numNodesInState_
-
-numNodes = 15
-numEdges = 50
-
-G = generateRandomGraph(numNodes, numEdges=numEdges)
-
-exposureRate = 10
-infectionRate = 2
-recoveryRate = .5
-
-log,stats = seirSim(G,exposureRate,infectionRate,recoveryRate,logSim=True, 
-              tallyFuncs=[numNodesInState(0),numNodesInState(1),
-                          numNodesInState(2),numNodesInState(3)])
-
-for i in range(4):
-    plt.plot(range(len(log)),stats[:,i])
-plt.legend(['Susceptible','Exposed','Infected','Removed'],loc='upper right')
-    
-    
-    
